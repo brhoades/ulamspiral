79a5192f83e3ac08ed8ebd197a219cbd2a99e00e
Billy Rhoades <billyarhoades@gmail.com>
2015-01-23 11:16:44 -0600

    Caching primes instead of generating them for every test

--- numspiral.rb
+++ numspiral.rb
@@ -4,6 +4,7 @@ require 'prime'
 
 width = ARGV[0].to_i
 height = ARGV[1].to_i
+
 img = Magick::Image.new(width, height)
 x = width/2-1
 y = height/2-1
@@ -11,16 +12,33 @@ y = height/2-1
 lensize = 1
 leniter = 2
 timesatlen = 1
-dir = "r"
-i = 1 
 
 turn = {"l" => "d", "d" => "r", "u" => "l", "r" => "u"}
+i = 1 
+dir = "r"
 
+class Fixnum
+  def prime?
+    return false if self % 2 == 0
+    max = Math.sqrt(self).floor
+
+    $cache.each do |i|
+      return true if i > max 
+      return false if self % i == 0  
+    end
+  end
+end
+
+print "Generating prime cache: "
+$cache = Prime.first Math.sqrt( width * height ).floor
+print "DONE\n"
+
+print "Creating picture: "
 while true
   break if x > width or x < 0
   break if y > height or y < 0
 
-  if Prime.prime? i 
+  if i.prime? 
     img.pixel_color(x, y, "rgb(0,0,0)")
   end
 
@@ -51,8 +69,10 @@ while true
       timesatlen = 2
     end
   end
-  
-  
 end
 
+print "DONE\n"
+
+print "Writing image: "
 img.write("demo-#{width}x#{height}.bmp")
+print "DONE\n"
